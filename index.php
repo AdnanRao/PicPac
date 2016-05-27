5862e69a062dff7581a4242246f0b622884cc3d1
AdnanRao <AdnanRao@users.noreply.github.com>
2016-05-27 16:24:48 +1000

    Create index.php
    PicPac - Image Hosting Script
    Index file
    If you need to contact author can do so by commenting here or visiting http://paksharez.com

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>PicPac - Image Hosting Script</title>
+</head>
+<body>
+
+  <form action="" method="" enctype="multiform/form-data">
+    <input type="file" name="pic" />
+    <input type="submit" value="Upload!" />
+  </form>
+
+</body>
+</html>
